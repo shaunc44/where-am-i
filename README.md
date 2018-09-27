093d7914f8d6b6f5bb4a7894c77a9a36941cf469
Shaun <seyyes2000@hotmail.com>
2018-09-27 17:28:28 -0400

    state server

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# where-am-i
+# Find US State using Coordinates  
+
+## Instructions  
+1. Download files  
+2. Install requirements from requirements.txt  
+3. Go to the where-am-i directory  
+4. Run ```python3 state_server.py```  
+5. Enter longitude  
+6. Enter latitude  
+
+### Expected Output (Example)  
+```
+Please enter a longitude:  -77.036133
+Please enter a latitude:  40.513
+That longitude and latitude is located in:   Pennsylvania
+```
+
+
+
---
+++ state_server.py
@@ -0,0 +1,77 @@
+import requests
+import json
+
+from shapely.geometry import Polygon, Point
+
+
+
+def get_user_coords():
+    longitude = input("\nPlease enter a longitude:  ")
+    latitude = input("\nPlease enter a latitude:  ")
+    # ERROR CHECK LAT AND LONG SOMEWHERE IN HERE ********
+    user_coords = {
+        'longitude': float(longitude),
+        'latitude': float(latitude)
+    }
+    return user_coords
+
+
+def get_state_data():
+    with open("states.json") as state_json:
+        state_data = json.load(state_json)
+        return state_data
+
+
+def check_coords():
+    user_coords = get_user_coords()
+    state_data = get_state_data()
+    provided_coord = Point(user_coords['longitude'], user_coords['latitude'])
+
+    for state in state_data:
+        # Create a polygon based on the border coordinates provided
+        state_borders = Polygon(state['border'])
+
+        if state_borders.contains(provided_coord):
+            print ("\nThat longitude and latitude is located in:  ", state['state'])
+            return state['state']
+    # return None
+    # return "Provided coordinates not found"
+    print ("\nProvided coordinates not found")
+    return None
+
+
+check_coords()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    # response = requests.post('http://127.0.0.1:8080/', data = user_coords)
+    # print(response.json())
+    # print (response.text)
+
+    # print ("state_data", state_data)
+    # session = requests.Session()
+    # response = session.post('http://127.0.0.1:8080/', data=user_coords)
+    # print ("response", response.text)
+
