113877b6513a803b54ecb68a1ef02e03a0e0dc3f
Shaun <seyyes2000@hotmail.com>
2018-09-30 21:45:44 -0400

    finish state server

--- README.md
+++ README.md
@@ -1,19 +1,29 @@
-# Find US State using Coordinates  
+# Find US State with Longitude & Latitude  
 
 ## Instructions  
 1. Download files  
-2. Install requirements from requirements.txt  
-3. Go to the where-am-i directory  
-4. Run ```python3 state_server.py```  
+2. Run ```pip install -r requirements.txt```  
+3. Go to the 'where-am-i' directory  
+4. Run ```python state_server.py```  
+5. Open a second terminal  
+6. Run ```python state_client.py```  
 5. Enter longitude  
 6. Enter latitude  
 
-### Expected Output (Example)  
+## Expected Output (Example)  
 ```
 Please enter a longitude:  -77.036133
 Please enter a latitude:  40.513
-That longitude and latitude is located in:   Pennsylvania
+ Pennsylvania
 ```
-
-
+```
+Please enter a longitude:  -77.7500915
+Please enter a latitude:  35.61542232
+ North Carolina
+```
+```
+Please enter a longitude:  25.89
+Please enter a latitude:  108.88
+ Provided coordinates not found
+```
 
--- state_server.py
+++ state_server.py
@@ -1,110 +1,39 @@
-import requests
 import json
-import ast
-
 from shapely.geometry import Polygon, Point
 from flask import Flask, request
 
 
-# Start Flask server
+# Start Flask
 app = Flask(__name__)
 @app.route('/', methods=['POST'])
 
 
-# def get_state_data():
-#     with open("states.json") as state_json:
-#         state_data = json.load(state_json)
-#         return state_data
-
-
 def result():
     if request.method == 'POST':
-        # state_data = get_state_data()
         user_coords = request.json
-
-        # print ("longitude", user_coords['longitude'])
-        # print ("latitude", user_coords['latitude'])
-        provided_coord = Point(
+        # Convert longitude and latitude to shapely Point
+        user_provided_coord = Point(
             user_coords['longitude'], 
             user_coords['latitude']
         )
 
+        # Get state data from JSON file
         with open("states.json") as state_json:
             state_data = json.load(state_json)
-
+            # Iterate over state data
             for state in state_data:
                 # Create a polygon based on the border coordinates provided
                 state_borders = Polygon(state['border'])
-
-                if state_borders.contains(provided_coord):
-                    # print (
-                    #     "\nThat longitude & latitude is located in: ", 
-                    #     state['state']
-                    # )
-                    print (state['state'])
+                # Check if coords are located within state's borders
+                if state_borders.contains(user_provided_coord):
                     return state['state']
-        # print ("user_coords:", user_coords)
-        # return 'Received!' # response to your request.
-        print ("Provided coordinates not found")
+        # If coordinates not located within any of the state's 
+        # polygonal borders return not found
         return "Provided coordinates not found"
-        # return None
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
-# app.run()
 
 
-
-
-
-
-# def get_user_coords():
-#     longitude = input("\nPlease enter a longitude:  ")
-#     latitude = input("\nPlease enter a latitude:  ")
-#     # ERROR CHECK LAT AND LONG SOMEWHERE IN HERE ********
-#     user_coords = {
-#         'longitude': float(longitude),
-#         'latitude': float(latitude)
-#     }
-#     return user_coords
-
-
-
-
-
-# def check_coords():
-#     user_coords = get_user_coords()
-#     state_data = get_state_data()
-#     provided_coord = Point(user_coords['longitude'], user_coords['latitude'])
-
-#     for state in state_data:
-#         # Create a polygon based on the border coordinates provided
-#         state_borders = Polygon(state['border'])
-
-#         if state_borders.contains(provided_coord):
-#             print ("\nThat longitude and latitude is located in:  ", state['state'])
-#             return state['state']
-#     # return None
-#     # return "Provided coordinates not found"
-#     print ("\nProvided coordinates not found")
-#     return None
-
-
-# check_coords()
-
-
-
-
-
-
-    # headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    # response = requests.post('http://127.0.0.1:8080/', data = user_coords)
-    # print(response.json())
-    # print (response.text)
-
-    # print ("state_data", state_data)
-    # session = requests.Session()
-    # response = session.post('http://127.0.0.1:8080/', data=user_coords)
-    # print ("response", response.text)
 
